7507494c2148c319ab62cc277e1e3020ac2cecb7
HaruS0610 <h85034588@gmail.com>
2026-08-11 09:14:53 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,46 +4,44 @@ namespace menuButtons {
     let action1: () => void = null
     let action2: () => void = null
 
-    //% block="2つのボタンを作る %label1 %label2"
-    export function createButtons(label1: string, label2: string) {
-        labels = [label1, label2]
-        selected = 0
-
-        controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-            selected = 0
-            draw()
-        })
-
-        controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-            selected = 1
-            draw()
-        })
-
-        controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-            if (selected == 0 && action1) {
-                action1()
-            }
-
-            if (selected == 1 && action2) {
-                action2()
-            }
-        })
-
-        draw()
-    }
-
-    function draw() {
+    function drawMenu() {
         screen.fill(0)
 
         for (let i = 0; i < labels.length; i++) {
-            let y = 35 + i * 30
+            let y = 40 + i * 30
 
             if (i == selected) {
                 screen.print(">", 20, y)
             }
 
-            screen.print(labels[i], 35, y)
+            screen.print(labels[i], 40, y)
         }
+    }
+
+    //% block="2つのボタンを作る %label1 %label2"
+    export function createButtons(label1: string, label2: string) {
+        labels = [label1, label2]
+        selected = 0
+
+        drawMenu()
+
+        controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+            selected = 0
+            drawMenu()
+        })
+
+        controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+            selected = 1
+            drawMenu()
+        })
+
+        controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+            if (selected == 0 && action1) {
+                action1()
+            } else if (selected == 1 && action2) {
+                action2()
+            }
+        })
     }
 
     //% block="ボタン1がAで選択されたとき"
