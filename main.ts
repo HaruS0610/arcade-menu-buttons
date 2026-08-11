370e5b4e25db119a1472c2cb9a3bbc00486d35de
HaruS0610 <h85034588@gmail.com>
2026-08-12 08:24:19 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,77 +1,86 @@
 namespace menuButtons {
     let selected = 0
     let labels: string[] = []
-    let buttons: TextSprite[] = []
-    let action1: () => void = null
-    let action2: () => void = null
+    let action1: (() => void) = null
+    let action2: (() => void) = null
+
+    let menuImage: Image = null
+    let menuSprite: Sprite = null
     let started = false
 
-    class TextSprite extends sprites.ExtendableSprite {
-        constructor(text: string) {
-            super(image.create(80, 24))
-            this.image.fill(0)
-            this.image.print(text, 5, 6)
+    const MenuKind = SpriteKind.create()
+
+    function drawMenu() {
+        if (!menuImage) {
+            return
+        }
+
+        menuImage.fill(0)
+
+        for (let i = 0; i < 2; i++) {
+            let y = 25 + i * 35
+
+            // ボタンの枠
+            menuImage.drawRect(25, y, 110, 25, 1)
+
+            // 選択中のカーソル
+            if (i == selected) {
+                menuImage.print(">", 32, y + 8, 1)
+            }
+
+            // ボタンの文字
+            menuImage.print(labels[i], 50, y + 8, 1)
         }
     }
 
-    function redraw() {
-        for (let i = 0; i < buttons.length; i++) {
-            let img = image.create(80, 24)
-            img.fill(0)
+    function setupControls() {
+        if (started) {
+            return
+        }
 
-            if (i == selected) {
-                img.drawRect(0, 0, 79, 23)
-                img.print(">", 4, 6)
-                img.print(labels[i], 15, 6)
-            } else {
-                img.print(labels[i], 15, 6)
+        started = true
+
+        controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+            selected = 0
+            drawMenu()
+        })
+
+        controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+            selected = 1
+            drawMenu()
+        })
+
+        controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+            if (selected == 0 && action1) {
+                action1()
             }
 
-            buttons[i].setImage(img)
-            buttons[i].setPosition(80, 45 + i * 30)
-        }
+            if (selected == 1 && action2) {
+                action2()
+            }
+        })
     }
 
     //% block="2つのボタンを作る %label1 %label2"
+    //% label1.defl="1"
+    //% label2.defl="2"
     export function createButtons(label1: string, label2: string) {
         labels = [label1, label2]
         selected = 0
 
-        for (let b of buttons) {
-            b.destroy()
+        menuImage = image.create(160, 100)
+
+        drawMenu()
+
+        if (menuSprite) {
+            menuSprite.destroy()
         }
 
-        buttons = []
+        menuSprite = sprites.create(menuImage, MenuKind)
+        menuSprite.setPosition(80, 60)
+        menuSprite.z = 1000
 
-        let button1 = new TextSprite(label1)
-        let button2 = new TextSprite(label2)
-
-        buttons.push(button1)
-        buttons.push(button2)
-
-        redraw()
-
-        if (!started) {
-            started = true
-
-            controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-                selected = 0
-                redraw()
-            })
-
-            controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-                selected = 1
-                redraw()
-            })
-
-            controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-                if (selected == 0 && action1) {
-                    action1()
-                } else if (selected == 1 && action2) {
-                    action2()
-                }
-            })
-        }
+        setupControls()
     }
 
     //% block="ボタン1がAで選択されたとき"
